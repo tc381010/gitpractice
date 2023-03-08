3cd456baf6946a956330e35d4fb15a6a32cb4d3b
duke chen <tc381010@gmail.com>
2023-03-08 16:42:54 +0800

    增加水果循環

---
+++ 0308.js
@@ -0,0 +1,13 @@
+// let array1 = [1, 2, 3, 4, 5]
+// array1.forEach((value, index, array) => {
+//     array[index] = value*3
+// })
+
+// console.log(array1)
+
+let array1 = ['apple','orange','watermelon']
+
+for (let i = 0 ; i < array1.length ; i++ ){
+    console.log(array1[i])
+}
+
